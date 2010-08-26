0d9b2168c8c3c272f40d01f0d094ddd50a3f414c
smathew <smathew@shareone.com>
2010-08-26 10:16:14 -0500

    Final commit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def sign_in(user)
     user.remember_me!
     cookies[:remember_token] = { :value   => user.remember_token,
-                                 :expires => 20.years.from_now.utc }
+                                 :expires => 15.minutes.from_now.utc }
     self.current_user = user
   end
   
